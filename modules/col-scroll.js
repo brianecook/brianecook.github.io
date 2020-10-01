a499fdefd63b180d62d30785953044e413fd9fdd
Brian Cook <brianecook@gmail.com>
2020-10-01 11:54:36 -0600

    adds support for proportional option and normally scrolled footer

--- modules/col-scroll.js
+++ modules/col-scroll.js
@@ -1,24 +1,74 @@
 export default colScroll;
 
-function colScroll($scrollTracks, breakpoint = 1024) {
-  if (window.innerWidth >= breakpoint) {
+const defaultOptions = {
+  proportional: false
+}
 
+function colScroll($scrollTracks, options = defaultOptions, breakpoint = 1024) {
+  if (window.innerWidth >= breakpoint) {
+    const $body = document.getElementsByTagName('body')[0];
+    let $tallestTrack = 0;
+
+    $scrollTracks.forEach($track => {
+      if ($track.scrollHeight > $tallestTrack) {
+        $tallestTrack = $track;
+      }
+    })
+
+    // makes sure every page refresh starts at top of page
     if ('scrollRestoration' in window.history) {
       window.history.scrollRestoration = 'manual'
     }
 
     window.scrollTo(0, 0);
 
+    // resets col-scroll style when user returns to top of page
+    window.addEventListener('scroll', (e) => {
+      if (window.scrollY === 0) {
+        $body.style.overflowY = 'hidden';
+      }
+    });
+
+    // Handles wheel event
     document.getElementsByTagName('body')[0].addEventListener('wheel', e => {
-      handleScroll($scrollTracks, e.deltaY);
+      $scrollTracks.forEach($track => {
+        const isScrollZero = window.scrollY === 0;
+        const newScrollDepth = Math.ceil($track.scrollTop + e.deltaY);
+        const scrollableHeight = $tallestTrack.scrollHeight - $tallestTrack.getBoundingClientRect().height;
+        const scrollProportion = e.deltaY / scrollableHeight;
+        let tracksScrolled;
+    
+        if (isScrollZero) {
+          if (options.proportional) {
+            const scrollableTrackHeight = $track.scrollHeight - $track.getBoundingClientRect().height;
+            const trackProportion = scrollableTrackHeight * scrollProportion;
+            $track.scrollTop += trackProportion;
+          } else {
+            $track.scrollTop = newScrollDepth;
+          }
+          tracksScrolled = getTracksScrolled($scrollTracks);
+        }
+    
+        if (tracksScrolled) {
+          $body.style.overflowY = 'visible';
+        } else {
+          if (isScrollZero) {
+            $body.style.overflowY = 'hidden';
+          }
+        }
+      });
     });
   }
 }
 
-function handleScroll($elems, deltaY) {
+function getTracksScrolled($elems) {
+  const trackArray = [];
   $elems.forEach($elem => {
-    $elem.scrollTop = $elem.scrollTop + deltaY;
+    const scrollTop = $elem.scrollTop
+    const trackBottom = $elem.scrollHeight - $elem.getBoundingClientRect().height
+    trackArray.push(scrollTop >= trackBottom)
   });
+  return trackArray.indexOf(false) > -1 ? false : true;
 }
 
 // const handleTouch = () => {
